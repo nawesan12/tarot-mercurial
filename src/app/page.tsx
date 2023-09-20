90545b8cda5a9c3c0c72e04757e4d67dba8273ce
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-09-20 01:58:33 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Hello from "@/app/blog/hello.mdx";
+
 export default function Page() {
   return (
     <section className="flex flex-col items-center">
       <h1 className="text-4xl font-semibold">Tarot Mercurial</h1>
+
+      <Hello />
     </section>
   );
 }
